5b0ad10c12002a2927b2d59f30a10db1e34e3d26
David <Djggamer@outlook.com>
2025-02-09 13:40:19 +0100

    Made ze button rotate and did a spawn animation (wip)

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let dropdown1 = document.querySelector('.userOptions');
 let dropdown2 = document.querySelector('.menuLink');
 let userMenu = document.querySelector('.userMenu');
 let menuLinkDrop = document.querySelector('.menuLinkDrop');
+let imgRotate = document.querySelector('.userContent')
 
 dropdown1.onclick = function() {
     userMenu.classList.toggle('active');
+    dropdown1.classList.toggle('active');
 }
 
 dropdown2.onmouseover = function() {
