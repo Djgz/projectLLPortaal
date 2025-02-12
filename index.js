847d7df712f04bcade04fcba835b46a211fd651e
Bart van Beusekom <142197@ichthuscollege.nl>
2025-02-12 12:21:10 +0100

    almost fixed it

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 let dropdown1 = document.querySelector('.userOptions');
-let dropdown2 = document.querySelector('.menuLink');
 let userMenu = document.querySelector('.userMenu');
-let menuLinkDrop = document.querySelector('.menuLinkDrop');
-let imgRotate = document.querySelector('.userContent');
-let dropdownMenu = document.querySelector('.menuLink > a')
 
 dropdown1.onclick = function() {
     userMenu.classList.toggle('active');
     dropdown1.classList.toggle('active');
 }
 
-dropdown2.onmouseover = function() {
-    menuLinkDrop.classList.toggle('active');
-}
-
-dropdown2.onmouseout = function() {
-    menuLinkDrop.classList.remove('active');
-}
-
-dropdownMenu.onclick
