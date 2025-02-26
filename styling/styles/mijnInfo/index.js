c120533996bfcfabc7a7d6f16402302ce742f04f
David <166148509+Djgz@users.noreply.github.com>
2025-02-26 21:50:46 +0100

    small updates

--- styling/styles/mijnInfo/index.js
+++
@@ -1,5 +0,0 @@
-let dropdown = document.querySelector('.userOptions');
-let userMenu = document.querySelector('.userMenu');
-dropdown.onclick = function() {
-    userMenu.classList.toggle('active');
-}
